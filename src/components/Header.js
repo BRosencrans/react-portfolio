24e89d8ddb17e3b027a5dea3729d9ee999945d06
BRosencrans <brandon.rosencrans@gmail.com>
2023-03-03 21:36:43 -0800

    added basic about me section

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom';
     return(
         <header>
             <h2 className="text-center">Welcome to my Portfolio!</h2>
-            
+
             <ul className="nav justify-content-around ">
+            
                 <li className="nav-item"><Link to="/">My Portfolio</Link></li>
                 <li className="nav-item"><Link to="/aboutMe">About Me</Link></li>
                 <li className="nav-item"><Link to="/resume">My Resume</Link></li>
                 <li className="nav-item"><Link to="/contactMe">Send me an Email</Link></li>
                    
-                </ul>
+            </ul>
            
         </header>
     )
---
+++ src/components/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function About() {
+    return (
+        <main className='text-center'>
+            <h3>About Me!</h3>
+            <div className='row'>
+                <div className='col-md-2'></div>
+                <div className='col-md-8'>
+                    <div className='about-text text-start'>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae.</p>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae..</p>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae.</p>
+                    </div>
+                </div>
+                <div className='col-md-2'></div>
+            </div>
+        </main>
+    )
+}
+
+export default About;
